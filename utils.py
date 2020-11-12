7a9c1a66284cd389a3d8443af40639197c265ce7
aseem09 <aseemverma999@gmail.com>
2020-11-12 23:44:42 +0530

    Fix vol issues

--- utils.py
+++ utils.py
@@ -108,13 +108,13 @@ def drop_path(x, drop_prob):
   return x
 
 
-# def create_exp_dir(path, scripts_to_save=None):
-  # if not os.path.exists(path):
-  #   os.mkdir(path)
-  # print('Experiment dir : {}'.format(path))
+def create_exp_dir(path, scripts_to_save=None):
+  if not os.path.exists(path):
+    os.mkdir(path)
+  print('Experiment dir : {}'.format(path))
 
-  # if scripts_to_save is not None:
-  #   os.mkdir(os.path.join(path, 'scripts'))
-  #   for script in scripts_to_save:
-  #     dst_file = os.path.join(path, 'scripts', os.path.basename(script))
-  #     shutil.copyfile(script, dst_file)
+  if scripts_to_save is not None:
+    os.mkdir(os.path.join(path, 'scripts'))
+    for script in scripts_to_save:
+      dst_file = os.path.join(path, 'scripts', os.path.basename(script))
+      shutil.copyfile(script, dst_file)
